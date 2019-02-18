e3f50a63d9d33831e34489e693f1ecec43250d2c
Harsimransingh Bindra <harsimransingh.bindra@colorado.edu>
2019-02-17 22:53:39 -0700

    Updates

--- Assignment-3/Question_3/kernal_mod.c
+++ Assignment-3/Question_3/kernal_mod.c
@@ -6,7 +6,9 @@
 * Description: Source file for kernal module to run a kernal timer
 _._._._._._._._._._._._._._._._._._._._._.*/
 
-
+/*reference: https://www.ibm.com/developerworks/library/l-timers-list/index.html
+ *           https://stackoverflow.com/questions/4039602/example-for-kernel-timer-implementation-in-linux-in-kernel-2-6-32
+ *           */
 
 #include <linux/module.h>
 #include <linux/moduleparam.h>
@@ -28,29 +30,29 @@ MODULE_PARM_DESC(timeout_value, "Kernel Timeout");
 module_param(user_name, charp, S_IWUSR | S_IXUSR | S_IRUSR);
 MODULE_PARM_DESC(user_name, "User's name");
 
-struct timer_list myTimer;
+struct timer_list user_timer;
 
-static void time_handler(void)
+static void trigger_function(void)
 {   counter++;
     printk(KERN_INFO "Name: %s, Triggered count: %d", user_name, counter);
-    mod_timer(&myTimer, jiffies + msecs_to_jiffies(timeout_value));
+    mod_timer(&user_timer, jiffies + msecs_to_jiffies(timeout_value));
 }
 
 static int __init my_module_init(void)
 {
     printk(KERN_INFO "my_module_init\n");
-    init_timer(&myTimer);
-    myTimer.data = (unsigned long)0;
-    myTimer.expires = jiffies + msecs_to_jiffies(timeout_value);
-    myTimer.function = (void(*)(unsigned long))time_handler;
-    add_timer(&myTimer);
+    init_timer(&user_timer);
+    user_timer.data = (unsigned long)0;
+    user_imer.function = (void(*)(unsigned long))trigger_function;
+    user_timer.expires = jiffies + msecs_to_jiffies(timeout_value);
+    add_timer(&user_timer);
     return 0;
 }
 
 static void __exit my_module_exit(void)
 {
     printk("Exiting my module\n");
-    del_timer(&myTimer);
+    del_timer(&user_timer);
     return;
     
 }
